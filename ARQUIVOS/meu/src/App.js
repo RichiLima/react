912e7a408637ce06a44de08afb631dfdc9a1c5e7
Richard Lima <112436267+RichiLima@users.noreply.github.com>
2023-01-12 16:51:58 -0300

    criação do meu projeto react

---
+++ ARQUIVOS/meu/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá Mundo!!!</h1>
+      <p>Meu primeiro app</p>
+    </div>
+  );
+}
+
+export default App;

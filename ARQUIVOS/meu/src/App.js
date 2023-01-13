f1eae3be6f59560d8624567bd7bed335c3e9d123
Richard Lima <112436267+RichiLima@users.noreply.github.com>
2023-01-13 12:07:51 -0300

    aula de react

--- ARQUIVOS/meu/src/App.js
+++ ARQUIVOS/meu/src/App.js
@@ -2,10 +2,24 @@
 import './App.css';
 
 function App() {
+  const name = `Richard`
+  const newname = name.toUpperCase()
+ 
+
+  function som(a, b) {
+    return a+b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
+
   return (
     <div className="App">
-      <h1>Olá Mundo!!!</h1>
+      <h1>Olá {newname}!!!</h1>
+      <h2>Alterando o JSX</h2>
       <p>Meu primeiro app</p>
+      <p>soma: {som(1, 5)}</p>
+      <img src={url} alt='Minha imagem'/>
     </div>
   );
 }
